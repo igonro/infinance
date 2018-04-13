cb7a33fd31e0d2f0b76d493aab327cbf1da8aade
Gabriel <100376343@alumnos.uc3m.es>
2018-04-13 12:34:36 +0200

    random string

---
+++ src/model/UserInfo.java
@@ -0,0 +1,12 @@
+package model;
+
+public class UserInfo {
+
+	private String name;
+	private String userID;
+	
+	public UserInfo(String name, String userID){
+		this.name = name;
+		this.userID = userID;
+	}
+}
